e2a690a17574c8df4672d4b7cae438f32436e3ad
maruyu998 <maruyu998@gmail.com>
2021-04-18 20:33:52 +0900

    added required modules

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
 	version="1.0",
 	description="atcoder auto run",
 	url="https://github.com/maruyu998/atc",
-	install_requires=["python-dotenv"],
+	install_requires=["python-dotenv", "requests", "python-dateutil"],
 	packages=find_packages(),
 	entry_points={
 		"console_scripts":[
